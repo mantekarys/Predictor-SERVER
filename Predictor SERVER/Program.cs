86e9abbd28f95d5916d9283215ffc369fdbcfe60
mantekarys <46086217+mantekarys@users.noreply.github.com>
2022-10-02 12:56:45 +0300

    progress

--- Predictor SERVER/Program.cs
+++ Predictor SERVER/Program.cs
@@ -18,12 +18,13 @@ using System.Threading;
 using Predictor_SERVER.Server;
 using System.Text.RegularExpressions;
 using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using System.Reflection;
 
 namespace Predictor_SERVER
 {
     public static class Variables
     {
-        public static List<Class> classes = new List<Class>(); //{ new Class(15, 10, 5, 1, 343, 10), new Class(15, 10, 5, 1, 343, 685) };
+        public static List<List<Class>> classes = new List<List<Class>>(); //{ new Class(15, 10, 5, 1, 343, 10), new Class(15, 10, 5, 1, 343, 685) };
         //public static Class c1 = new Class(15, 10, 5, 1, 343, 10);//pradines coord pakeist nes paskui kai su walls buna keistai gal i speed?
         //public static Class c2 = new Class(15, 10, 5, 1, 343, 685);//kaska su tom class daryt
         public static List<MapObject> mapO = new List<MapObject>();
@@ -33,18 +34,23 @@ namespace Predictor_SERVER
         public static bool started = false;
 
         public static List<Server.Match> matches = new List<Server.Match>();
+        public static List<List<string>> matchIds = new List<List<string>>();
 
-        public static void SendMessages()
+        public static void SendMessages(int matchId)
         {
             Variables.started = true;
             Timer newTimer = new Timer();
-            newTimer.Elapsed += new ElapsedEventHandler(Broadcast);
+            newTimer.Elapsed += delegate { Broadcast(matchId); };//new ElapsedEventHandler(Broadcast,5);
             newTimer.Interval = 100;
             newTimer.Start();
         }
-        public static void Broadcast(object sender, EventArgs e)
+        public static void Broadcast(int matchId)//object sender, EventArgs e
         {
-            var message = JsonConvert.SerializeObject((Variables.classes.ToList(), Variables.map));
+            var message = JsonConvert.SerializeObject((Variables.classes[matchId].ToList(), Variables.map));
+            //foreach (var item in Variables.matchIds[matchId])
+            //{
+            //    sesions.SendTo(message, item);
+            //}
             sesions.Broadcast(message);
         }
 
@@ -85,33 +91,39 @@ namespace Predictor_SERVER
                 if (code == 752)//creates match
                 {
                     matchId = Variables.matches.Count;
+                    //Variables.matchIds.Add(new List<string>());
+                    //Variables.matchIds[matchId].Add(ID);
                     Variables.matches.Add(new Server.Match(matchId, text));
-                    var message = JsonConvert.SerializeObject((matchId, Variables.howMany++));
+                    Variables.classes.Add(new List<Class>());
+                    var message = JsonConvert.SerializeObject((matchId, Variables.matches[matchId].peopleAmount-1));
                     Send(message);
                 }
                 if (code == 876)////joined match
                 {
                     matchId= int.Parse(text);
-                    Variables.matches[matchId].peopleAmount++;
-                    var message = JsonConvert.SerializeObject(Variables.howMany++);
+                    //Variables.matchIds[matchId].Add(ID);
+                    var message = JsonConvert.SerializeObject(Variables.matches[matchId].peopleAmount++);
                     Send(message);
                 }
                 if (code == 545)//readies up
                 {
                     Variables.matches[matchId].ready += ready;
-                    Variables.classes.Add(ClassCreator.pickCreator(text, 15,which));
+                    Variables.classes[matchId].Add(ClassCreator.pickCreator(text, 15,which));
                     if (Variables.matches[matchId].ready == Variables.matches[matchId].peopleAmount)
                     {
-                        var message = JsonConvert.SerializeObject((Variables.classes.ToList(), Variables.map));
+                        var message = JsonConvert.SerializeObject((Variables.classes[matchId].ToList(), Variables.map));
 
                         if (!Variables.started)//gal istrint ta if ir cia apskritai siuncia vissiems o turetu tik tiems kuriu matchas
                         {
 
-                            ThreadStart childref = new ThreadStart(Variables.SendMessages);
-                            Thread childThread = new Thread(childref);
-                            childThread.Start();
+                            //ThreadStart childref = new ThreadStart(Variables.SendMessages);
+                            //Thread childThread = new Thread(childref);
+                            //childThread.Start();
+
+                            var thread = new Thread(
+                                () => Variables.SendMessages(matchId));
+                            thread.Start();
                             Variables.sesions = Sessions;
-                            Variables.SendMessages();
                         }
                         //Send(message);
                         Sessions.Broadcast(message);
@@ -126,9 +138,8 @@ namespace Predictor_SERVER
             }
             if (e.Data == "159")//map start
             {
-
-                List<MapObject> mapO = new List<MapObject>();
-                var message = JsonConvert.SerializeObject((Variables.classes, Variables.map, Variables.howMany++));
+                Variables.classes.Add(new List<Class>() { new Class(15, 10, 5, 1, 343, 10), new Class(15, 10, 5, 1, 343, 685) });
+                var message = JsonConvert.SerializeObject((Variables.classes[0], Variables.map, Variables.howMany++));
                 if (Variables.howMany > 2)
                 {
                     Variables.howMany = 0;
@@ -136,13 +147,17 @@ namespace Predictor_SERVER
                 if (!Variables.started)
                 {
 
-                    ThreadStart childref = new ThreadStart(Variables.SendMessages);
-                    Thread childThread = new Thread(childref);
-                    childThread.Start();
+                    //ThreadStart childref = new ParameterizedThreadStart(Variables.SendMessages);
+                    //Thread childThread = new Thread(childref);
+                    //childThread.Start(matchId);
+                    //Variables.sesions = Sessions;
+
+                    var thread = new Thread(
+                         () => Variables.SendMessages(0));
+                    thread.Start();
                     Variables.sesions = Sessions;
-                    Variables.SendMessages();
                 }
-                Send(message);
+                //Send(message);
             }
             if (e.Data == "999")//match list
             {
@@ -169,10 +184,11 @@ namespace Predictor_SERVER
             MouseEventArgs mouseClick;
             int which;
             var map = Variables.map;
-            (keys, mouseClick, which) = JsonConvert.DeserializeObject<(HashSet<Keys>, MouseEventArgs mouseClickp, int)>(e.Data);
+            int matchId;
+            (keys, mouseClick, which, matchId) = JsonConvert.DeserializeObject<(HashSet<Keys>, MouseEventArgs mouseClickp, int, int)>(e.Data);
 
 
-            var c = Variables.classes[which];//sita reik susirast kuris siunte ir galbut iskart dirbt su listu
+            var c = Variables.classes[matchId][which];//sita reik susirast kuris siunte ir galbut iskart dirbt su listu
             int pad = 5;
             foreach (var keyData in keys)
             {
@@ -222,10 +238,12 @@ namespace Predictor_SERVER
                     }
                 }
             }
-            Variables.classes[which] = c;
+            Variables.classes[matchId][which] = c;
 
-            var message = JsonConvert.SerializeObject((Variables.classes.ToList(), Variables.map));
+            var message = JsonConvert.SerializeObject((Variables.classes[matchId].ToList(), Variables.map));
             Variables.sesions = Sessions;
+            
+            var z = this.Context;
             //Sessions.Broadcast(message);
 
         }
@@ -244,11 +262,9 @@ namespace Predictor_SERVER
 
 
             WebSocketServer wssv = new WebSocketServer("ws://127.0.0.1:7890");
-
+   
             wssv.AddWebSocketService<Echo>("/Echo");
             wssv.AddWebSocketService<EchoAll>("/EchoAll");
-            
-
             wssv.Start();
             Console.WriteLine("WS server started on ws://127.0.0.1:7890/Echo");
             Console.WriteLine("WS server started on ws://127.0.0.1:7890/EchoAll");
