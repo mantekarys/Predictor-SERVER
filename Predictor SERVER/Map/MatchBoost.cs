9525b96402fa38df21a6c9f2276ff3ffa6bd4117
maneris <mantkveder@gmail.com>
2022-10-26 00:02:33 +0300

    Created base Facade Class missing it's use implementation

---
+++ Predictor SERVER/Map/MatchBoost.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Predictor_SERVER.Character;
+
+namespace Predictor_SERVER.Map
+{
+    internal class MatchBoost
+    {
+        private Class _player;
+        public MatchBoost(Class player)
+        {
+            this._player = player;
+        }
+        /// <summary>
+        /// method for creation of an item and apllying it to player class
+        /// </summary>
+        public void ItemBoost()
+        {
+            Item item = new AttackSpeedPotion((0, 0));
+            item.pickedUp(_player);
+        }
+        /// <summary>
+        /// method for creation of powerup and apllying it to player class
+        /// </summary>
+        public void PowerUpBoost()
+        {
+            PowerUp powerUp = new AttackSpeedPowerUp((0,0));
+            powerUp.pickedUp(_player);
+        }
+    }
+}
